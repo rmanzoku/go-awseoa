7ec23f96b926c18d1377c5b2122120dafbc202d4
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-06-19 12:44:59 +0000

    Use assert instead of is

--- awseoa_test.go
+++ awseoa_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/cheekybits/is"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
 
@@ -36,25 +36,25 @@ func TestCreateSigner(t *testing.T) {
 	if os.Getenv("CREATE") == "" {
 		t.Skip()
 	}
-	is := initTesting(t)
+	initTesting(t)
 	s, err := CreateSigner(svc)
 	fmt.Println(err)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	fmt.Println(s.Address().String())
 }
 
 func TestSetAlias(t *testing.T) {
-	is := initTesting(t)
+	initTesting(t)
 	s, err := NewSigner(svc, keyID)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	err = s.SetAlias(s.Address().String())
-	is.Nil(err)
+	assert.Nil(t, err)
 }
 
 func TestSendEther(t *testing.T) {
-	is := initTesting(t)
+	initTesting(t)
 
 	topts.GasPrice, _ = new(big.Int).SetString("1000000000", 10)
 	topts.Context = context.TODO()
@@ -62,48 +62,48 @@ func TestSendEther(t *testing.T) {
 	amount, _ := new(big.Int).SetString("1000000000000", 10)
 
 	ethcli, err := ethclient.Dial(rpc)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	tx, err := SendEther(ethcli, topts, to, amount)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	fmt.Println(tx.Hash().String())
 }
 
 func TestEthereumSign(t *testing.T) {
-	is := initTesting(t)
+	initTesting(t)
 
 	s, err := NewSigner(svc, keyID)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	msg := "0xd75be5d1b23bc1c3c22c0708a5c822f927f1eb8d609d684ef91996fd2bf2bbda"
 	msgb, err := decodeHex(msg)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	hash := toEthSignedMessageHash(msgb)
 
 	sig, err := s.EthereumSign(msgb)
-	is.Nil(err)
+	assert.Nil(t, err)
 
 	fmt.Println(s.Address().String())
 	fmt.Println(encodeToHex(sig))
 
 	addr, err := recover(hash, sig)
+	assert.Nil(t, err)
 	fmt.Println(addr.String())
 }
 
-func initTesting(t *testing.T) is.I {
-	is := is.New(t)
+func initTesting(t *testing.T) {
 	sess, err := session.NewSessionWithOptions(session.Options{
 		Config:  aws.Config{Region: aws.String(region)},
 		Profile: profile,
 	})
 
-	is.Nil(err)
+	assert.Nil(t, err)
 	svc = kms.New(sess)
 
 	topts, err = NewKMSTransactor(svc, keyID)
-	is.Nil(err)
+	assert.Nil(t, err)
 
-	return is
+	return
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.29.33
-	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927
 	github.com/ethereum/go-ethereum v1.9.12
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 )
